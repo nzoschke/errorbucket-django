9096ff6eecb4b87497829dc6b939b06e0fa22854
Noah Zoschke <noah@heroku.com>
2010-02-11 12:16:41 -0800

    enable admin site with ErrorBucket branding

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 # Django settings for errorbucket project.
+import os
+PROJECT_ROOT = os.path.dirname(__file__)
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -75,6 +77,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, "templates")
 )
 
 INSTALLED_APPS = (
@@ -83,4 +86,5 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+    'django.contrib.admin',
 )
