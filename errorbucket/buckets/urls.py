f1ce893316a13e5aa58010d7248238f89126f355
Noah Zoschke <noah@heroku.com>
2010-02-17 06:57:59 -0800

    django app not as useful on GAE

--- errorbucket/buckets/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('errorbucket.buckets.views',
-    (r'^$', 'buckets'),
-    (r'^(?P<name>[\w ]+)/$', 'bucket'),
-    (r'^(?P<name>[\w ]+)/errors/$', 'errors'),
-)
